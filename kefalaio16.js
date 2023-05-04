ff85df3c503520270fe560ef823ecda5c189f0d1
TOMMY-MESSI <117346977+TOMMY-MESSI@users.noreply.github.com>
2023-05-04 11:32:03 +0300

    Add files via upload

---
+++ kefalaio16.js
@@ -0,0 +1,46 @@
+/* Εφαρμογή 16: Κώδικας JavaScript για την δυναμική μετατροπή
+* περιεχομένου της ιστοσελίδας
+* Αρχείο p16.js 
+*/
+function Toyota_Supra() {
+document.getElementById("περιεχόμενο").innerHTML = 
+'<p><a href="https://www.toyota.com/grsupra/" target="_blank">ΤΙΜΗ</a></p> '+
+'<p><a href="https://www.toyota.gr/" target="_blank">ΑΝΤΙΠΡΟΣΟΠΙΑ </a></p> '+
+'<p><a href="https://www.toyota.gr/buy-and-offers/financing?gclid=EAIaIQobChMIxo2I0Z7M_gIVidJ3Ch1FMAnIEAAYASADEgK9V_D_BwE" target="_blank">ΧΡΗΜΑΤΟΔΟΤΙΚΑ ΠΡΟΓΡΑΜΜΑΤΑ</a></p> '
+;
+}
+function diastima() {
+document.getElementById("περιεχόμενο").innerHTML = 
+'<p><a href="https://www.bugatti.com/" target="_blank">ΑΝΤΙΠΡΟΣΟΠΙΑ </a></p> '+
+'<p><a href="https://www.bugatti.com/chiron/" target="_blank">ΤΙΜΗ</a></p> '+
+'<p><a href="https://www.bugatti.com/lifestyle/" target="_blank">lifestyle '
+;
+}
+function ekpaideysi() {
+document.getElementById("περιεχόμενο").innerHTML = 
+'<p><a href="https://www.ferrari.com/en-GR/auto/laferrari" target="_blank">ΤΙΜΗ</a></p> '+
+'<p><a href="https://www.ferrari.com/en-GR" target="_blank">ΑΝΤΙΠΡΟΣΟΠΙΑ</a></p> '+
+'<p><a href="https://www.ferrari.com/en-GR/auto/car-part-services" target="_blank">services</a></p> '
+;
+}
+function periballon() {
+document.getElementById("περιεχόμενο").innerHTML = 
+'<p><a href="https://www.mclaren.com/" target="_blank">ΑΝΤΙΠΡΟΣΟΠΙΑ</a></p> '+
+'<p><a href="https://cars.mclaren.com/en/750s-cars" target="_blank">ΤΙΜΗ</a></p> '+
+'<p><a href="https://group.mclaren.com/" target="_blank">Group</a></p> '
+;
+}
+function politismos() {
+document.getElementById("περιεχόμενο").innerHTML = 
+'<p><a href="https://www.ferrari.com/" target="_blank">ΑΝΤΙΠΡΟΣΟΠΙΑ</a></p> '+
+'<p><a href="https://www.ferrari.com/en-GR/auto/laferrari" target="_blank">ΤΙΜΗ</a></p> '+
+'<p><a href="https://www.ferrari.com/en-GR/auto/car-part-services" target="_blank">services</a></p> '
+;
+}
+function texnologia() {
+document.getElementById("περιεχόμενο").innerHTML = 
+'<p><a href="https://www.pagani.com/" target="_blank">ΑΝΤΙΠΡΟΣΟΠΙΑ</a></p> '+
+'<p><a href="https://www.pagani.com/zonda-r/" target="_blank">ΤΙΜΗ</a></p> '+
+'<p><a href="https://paganistore.com/it" target="_blank">Pgani store</a></p> '
+;
+}
